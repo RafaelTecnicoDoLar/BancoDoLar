dd0869d0e85144438d54a74e66320f8e5f471df1
RafaelTecnicoDoLar <rjonasmombellifranco@gmail.com>
2024-11-03 00:22:47 -0300

    asd

---
+++ Banco Do Lar/usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco_Do_Lar
+{
+    public partial class usuario : Form
+    {
+        public usuario()
+        {
+            InitializeComponent();
+
+            string usuario = "";
+            Console.WriteLine("Insira o Usuario");
+            usuario = Console.ReadLine();
+
+        }
+    }
+}
